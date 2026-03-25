f0b785fabc12edb67434f62918dabd99184be534
Diksha Turkar <turkardiksha345@gmail.com>
2026-03-25 19:28:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,25 @@
-resource "aws_db_subnet_group" "my_db_subnet" {
-  name = "my-rds-subnet-group"
 
-  subnet_ids = var.subnet_ids
+data "aws_vpc" "default" {
+  default = true
+}
+
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+data "aws_security_group" "default" {
+  name   = "default"
+  vpc_id = data.aws_vpc.default.id
+}
+
+
+resource "aws_db_subnet_group" "my_db_subnet" {
+  name       = "my-rds-subnet-group"
+  subnet_ids = data.aws_subnets.default.ids   # ✅ using default subnets
 
   tags = {
     Name = "my-rds-subnet-group"
@@ -9,21 +27,21 @@ resource "aws_db_subnet_group" "my_db_subnet" {
 }
 
 resource "aws_db_instance" "my_rds" {
-  identifier             = "my-rds-instance"   
-  engine                 = var.engine
-  engine_version         = var.engine_version
-  instance_class         = var.instance_class
-  allocated_storage      = 10
+  identifier              = "my-rds-instance"
+  engine                  = var.engine
+  engine_version          = var.engine_version
+  instance_class          = var.instance_class
+  allocated_storage       = 10
 
-  db_name                = var.db_name
-  username               = var.username
-  password               = var.password
+  db_name                 = var.db_name
+  username                = var.username
+  password                = var.password
 
-  db_subnet_group_name   = aws_db_subnet_group.my_db_subnet.name
-   
+  db_subnet_group_name    = aws_db_subnet_group.my_db_subnet.name
+  vpc_security_group_ids  = [data.aws_security_group.default.id]   
 
-  publicly_accessible    = false
-  skip_final_snapshot    = true
+  publicly_accessible     = false
+  skip_final_snapshot     = true
 
   tags = {
     Name = "my-db"
