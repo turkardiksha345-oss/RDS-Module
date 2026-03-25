5dd473834aab9ac82c7b094cb944eb9a0583eec4
Diksha Turkar <turkardiksha345@gmail.com>
2026-03-25 19:28:49 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -4,11 +4,9 @@ output "rds_endpoint" {
 }
 
 output "rds_id" {
-  description = "RDS instance ID"
-  value       = aws_db_instance.my_rds.id
+  value = aws_db_instance.my_rds.id
 }
 
 output "rds_arn" {
-  description = "RDS ARN"
-  value       = aws_db_instance.my_rds.arn
+  value = aws_db_instance.my_rds.arn
 }
